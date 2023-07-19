eee61dc2747ef9aba9954fe29d440691f2a6873d
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-07-19 23:36:38 +0200

    c

--- Coding/Ospedale/Query/RicoveroSenzaIntervento.sql
+++ Coding/Ospedale/Query/RicoveroSenzaIntervento.sql
@@ -1,9 +1,12 @@
 /*	Visualizzare le date di ricovero di tutti i pazienti che non hanno subito alcun intervento	*/
 
-SELECT		P.nome, PZ.data_ric
-FROM		PERSONA P JOIN PAZIENTE PZ ON(P.cf = PZ.cf)
-					  JOIN INTERVENTO I ON(P.cf = I.cf_paz)
-WHERE		P.cf NOT IN(I.cf_paz);
+SELECT	 PZ.cf, P.nome, P.cognome, PZ.data_ric
+FROM	PAZIENTE PZ JOIN PERSONA P ON(PZ.cf = P.cf)
+					LEFT JOIN INTERVENTO I ON(P.cf = I.cf_paz)
+WHERE	I.cf_paz IS NULL;
 
 
--- N.B: no data found
+-- INSERT: 
+--insert into PERSONA(CF,NOME,COGNOME,DATA_NASCITA,SESSO) values('AAA0000000010101','ROCCO','SCARPATO',to_date('18/10/1990','dd/mm/yyyy'),'M');
+--insert into PAZIENTE(DATA_RIC,DATA_DIM,CF) values(to_date('01/01/2023','dd/mm/yyyy'),to_date('05/01/2023','dd/mm/yyyy'),'AAA0000000010101');
+
