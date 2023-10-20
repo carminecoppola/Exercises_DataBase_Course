32bc87bea9a5cc0be4dda2d1fa76cf6889e0eb6b
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-10-20 15:38:26 +0200

    c

--- Coding/Ospedale/Trigger/trigger2.sql
+++ Coding/Ospedale/Trigger/trigger2.sql
@@ -94,3 +94,73 @@ begin
     		RAISE_APPLICATION_ERROR('-20002','already_done: il paziente ha gia effettuato gia un intervento di questo tipo');
 end;
 
+-------------------------------------------------
+
+/*  Un paziente non può subire più di due interventi in un mese. Questi devono avvenire a distanza
+    di non meno di una settimana uno dall'altro e non possono essere dello stesso tipo.
+    Eccezioni:
+        too_frequent, time_interval_too_short, already_done. 
+*/
+
+create or replace trigger controlloInterv before insert on intervento
+for each row
+declare
+
+    num_int_mensili			number;
+	num_int_sett			number;
+	num_tipo				number;
+
+    too_frequent			EXCEPTION;
+    time_interval_too_short	EXCEPTION;
+    already_done			EXCEPTION;
+    
+begin
+
+	--1)Mese
+    select	count(*)
+    into	num_int_mensili
+    from	paziente pz join persona p on pz.cf = p.cf
+    					join intervento i on p.cf = i.cf_paz
+	where	i.id =:NEW.id and i.data_e_ora <= SYSDATE - 31;
+
+
+	if num_int_mensili > 2	then
+        raise too_frequent;
+	end if;
+
+	--2)Settimana
+	select	count(*)
+    into	num_int_sett
+    from	paziente pz join persona p on pz.cf = p.cf
+    					join intervento i on p.cf = i.cf_paz
+    where	i.id =:NEW.id and i.data_e_ora <= SYSDATE - 7;
+        
+
+	if num_int_sett > 1	then
+        raise time_interval_too_short;
+	end if;
+
+
+	--3)Stesso Tipo
+
+	select	count(i.tipo)
+    into	num_tipo
+    from	paziente pz join persona p on pz.cf = p.cf
+    					join intervento i on p.cf = i.cf_paz
+    where	i.tipo =:NEW.tipo and i.data_e_ora <= SYSDATE - 7;
+
+	if num_tipo > 1 then
+        raise already_done;
+	end if;
+        
+    
+
+    EXCEPTION
+        WHEN too_frequent THEN
+    		raise_application_error('-20001','too_frequent: questo paziente ha gia subito 2 interventi questo mese');
+		WHEN time_interval_too_short THEN
+    		raise_application_error('-20002','time_interval_too_short: questo paziente ha gia subito un intervento questa settimana');
+		WHEN already_done THEN
+    		raise_application_error('-20003','already_done: questo paziente ha gia subito un interento di questo tipo in questa settimana');
+		
+end;
