e86cdbee2139ebc31772b9956dc4ce6689036182
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-10-22 18:38:25 +0200

    c

--- Coding/Ospedale/Query/Intervento2ore.sql
+++ Coding/Ospedale/Query/Intervento2ore.sql
@@ -9,6 +9,14 @@ FROM		PERSONA P JOIN PAZIENTE PZ ON(P.cf = PZ.cf)
 WHERE		(SYSDATE - I.data_e_ora > 7 AND I.durata > 2);
 
 
+select	p.cf ,p.nome, p.data_nascita, subq.cf_paz, subq.data_e_ora, subq.durata
+from	persona p join (
+                         select	 i.cf_paz,i.durata,i.data_e_ora
+                         from	 intervento i
+                         where	 i.durata > 2 and i.data_e_ora < sysdate - 7
+    					) subq on p.cf = subq.cf_paz
+order by subq.durata desc;
+
 
 --Dovrebbe essere più giusta con il popolamento aggiornato
 SELECT P.cf ,P.nome, P.data_nascita, P.sesso,subq.data_e_ora, subq.durata
@@ -19,4 +27,4 @@ SELECT P.cf ,P.nome, P.data_nascita, P.sesso,subq.data_e_ora, subq.durata
 		WHERE	IT.durata > 2
     )subq on subq.cf_paz = P.cf
 WHERE SYSDATE - subq.data_e_ora > 7
-order by subq.durata;
+order by subq.durata desc;
