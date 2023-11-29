5b85ff5d8d5e052f354ff878d4dcb97d6db50ac4
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-11-29 15:51:54 +0100

    corrette

--- Coding/Asta/Query/AsteDiverseConcluseLouvre.sql
+++ Coding/Asta/Query/AsteDiverseConcluseLouvre.sql
@@ -1,11 +1,34 @@
 /*  Visualizzare il nome e cognome degli utenti che nell'anno scorso (fa fede la data di sistema)
-    hanno partecipato solo ad aste su opere d'arte di provenienza 'LOUVRE' terminate con una vendita */
+    hanno partecipato solo ad aste su opere d'arte di provenienza 'LOUVRE' terminate con una vendita 
+
+	Dovrebbe ritornarmi solo GIANFRANCO
+*/
+
+SELECT 	DISTINCT u1.nome, u1.cognome, o1.provenienza
+FROM 	utente u1 JOIN 	rilanciata r1 ON u1.login = r1.login
+				  JOIN 	asta a1 ON r1.id_asta = a1.id_asta
+				  JOIN 	oggetto o1 ON a1.codice_oggetto = o1.codice_oggetto
+WHERE 	o1.provenienza = 'LOUVRE' 
+AND 	NOT EXISTS (
+            SELECT 1
+            FROM utente u2 
+            JOIN rilanciata r2 ON u2.login = r2.login
+            JOIN asta a2 ON r2.id_asta = a2.id_asta
+            JOIN oggetto o2 ON a2.codice_oggetto = o2.codice_oggetto
+            WHERE u1.login = u2.login AND a2.data_inizio >= SYSDATE - 365
+        )
+AND 	NOT EXISTS (
+            SELECT 1
+            FROM utente u3 
+            JOIN rilanciata r3 ON u3.login = r3.login
+            JOIN asta a3 ON r3.id_asta = a3.id_asta
+            JOIN oggetto o3 ON a3.codice_oggetto = o3.codice_oggetto
+            WHERE u1.login = u3.login AND o3.provenienza <> 'LOUVRE'
+        );
 
 
-select 	distinct u.nome,u.cognome, count(*) as aste_term_con_vendita
+-- Controlla con questo:
+select 	u.nome,u.cognome, o.provenienza
 from	utente u join rilanciata r on u.login = r.login
 				 join asta a on r.id_asta = a.id_asta
-    			 join vendita v on a.id_asta = v.id_asta
 				 join oggetto o on a.codice_oggetto = o.codice_oggetto
-where	o.provenienza = 'LOUVRE' and a.data_fine <= SYSDATE - 365
-group by u.nome,u.cognome;
--- Coding/Asta/Trigger/trigger3.sql
+++ Coding/Asta/Trigger/trigger3.sql
@@ -3,75 +3,7 @@
     se il massimo rilancio è superiore al prezzo base di almeno il 10%.
 */
 
-create or replace trigger controlliAsta before insert on asta
-for each row
-declare
-
-    num_rilanci			NUMBER;
-    max_rilancio		NUMBER;
-	r_cont_uguali		NUMBER;
-	p_base				NUMBER;
-
-    minimo_rilanci		EXCEPTION;
-	rilanci_uguali		EXCEPTION;
-	massimo_rilancio	EXCEPTION;
-
-begin
-
-    --Minimo 3 rilanci di utenti diversi 
-    
-	SELECT 	count(distinct r.login)
-    INTO 	num_rilanci
-    FROM 	rilanciata r
-    WHERE 	r.id_asta = :NEW.id_asta;
-
-	IF num_rilanci < 3 THEN
-        raise minimo_rilanci;
-	END IF;
-
-	--Rilanci uguali 
-
-	SELECT	count(*)
-    INTO	r_cont_uguali
-    FROM (	SELECT	 r.prezzo_rilancio, count(*) as num_rilanci
-        	FROM 	 rilanciata r
-        	WHERE	 r.id_asta =:NEW.id_asta
-        	GROUP BY r.prezzo_rilancio
-        	HAVING count(*) > 1
-    	 );
-
-	IF r_cont_uguali > 0 THEN
-        raise rilanci_uguali;
-	END IF;
-
-	-- Rilancio troppo basso
-	SELECT  max(r.prezzo_rilancio), a.prezzo_base
-    INTO	max_rilancio, p_base
-    FROM	rilanciata r join asta a on r.id_asta = a.id_asta
-    WHERE	a.id_asta =:NEW.id_asta;
-
-	IF max_rilancio < p_base * 0.10 THEN
-        raise massimo_rilancio;
-	END IF;
-
-	EXCEPTION
-    	when minimo_rilanci then
-    		raise_application_error('-20001','minimo_rilanci: asta non ha ricevuto almeno 3 rilanci');
-		when rilanci_uguali then
-    		raise_application_error('-20002','rilanci_uguali: il rilancio di questo prezzo e gia stato fatto');
-		when massimo_rilancio then
-    		raise_application_error('-20003','massimo_rilancio: non supera il 10% del prezzo base');
-		
-end;
-
-
-
--- /*  Un'asta si conclude con una vendita solo se ha avuto almeno 3 rilanci
-    da parte di utenti diversi, se non ci sono rilanci di pari importo e 
-    se il massimo rilancio è superiore al prezzo base di almeno il 10%.
-*/
-
-create or replace trigger ControlloAsta before insert on Asta
+create or replace trigger ControlloAsta before insert on Vendita
 for each row
 declare
 
@@ -90,10 +22,10 @@ begin
     /* 1) Un'asta si conclude con una vendita solo se ha avuto
    		  almeno 3 rilanci da parte di utenti diversi */
 
-    select  count(DISTINCT r.prezzo_rilancio)
+    select  count(DISTINCT r.login)
     into	num_ril
-    from 	asta a join rilanciata r on a.login <> r.login
-    where	a.id_asta =:NEW.id_asta;
+    from 	rilanciata r
+    where	r.id_asta =:NEW.id_asta;
 
     IF	num_ril > 2 THEN
     	RAISE minmio_rilanci;
@@ -103,8 +35,8 @@ begin
 
     select  count(*)
     into	pari_ril
-    from	asta a join rilanciata r on a.id_asta = r.id_asta
-    where	r.prezzo_rilancio =:NEW.prezzo_rilancio and a.id_asta =:NEW.id_asta;
+    from	rilanciata r1 join rilanciata r2 on r1.id_asta = r2.id_asta
+    where	r1.prezzo_rilancio = r2.prezzo_rilancio; -- Dovrebbe essere :=NEW.prezzo_rilancio ma da errore
 
     IF	pari_ril > 0 THEN
     	RAISE pari_rilanci;
@@ -113,12 +45,20 @@ begin
 
     -- 3) Massimo rilancio è superiore al prezzo base di almeno il 10%
 
-	SELECT 	MAX(r.prezzo_rilancio), a.prezzo_base
-    INTO	max_ril,pz_base
-    FROM	asta a join rilanciata r on a.id_asta = r.id_asta
-    WHERE	a.id_asta =:NEW.id_asta;
+	SELECT 	MAX(r.prezzo_rilancio)
+    INTO	max_ril
+    FROM	rilanciata r
+    WHERE	r.id_asta =:NEW.id_asta;
 
-	IF	max_rilancio <= pz_base * 0.10 THEN
+	SELECT 	prezzo_base
+    INTO	pz_base
+    FROM	asta a join rilanciata r on a.id_asta = r.id_asta
+    WHERE	r.prezzo_rilancio = max_ril;
+
+    -- Calcolo il prezzo base dell'oggetto messo all'asta grazie alla 
+    -- riga WHERE r.prezzo_rilancio = max_ril calcolato in precedenza
+
+	IF	max_ril <= pz_base * 0.10 THEN
     	RAISE max_rilancio;
 	END IF;
 
@@ -133,6 +73,3 @@ begin
 
 end;
 
-
-/* Errors: TRIGGER CONTROLLOASTA
-Line/Col: 32/30 PLS-00049: bad bind variable 'NEW.PREZZO_RILANCIO' */
