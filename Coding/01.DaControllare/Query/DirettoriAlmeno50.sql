13af202e537289f7eadcfbeba54b87da027cfbf5
carmine <carmine.coppola001@studenti.uniparthenope.it>
2023-12-22 18:09:09 +0100

    the end

--- Coding/01.DaControllare/Query/DirettoriAlmeno50.sql
+++
@@ -1,6 +0,0 @@
-/* Trovare il CF e il NOME dei direttori di dipartimento che lavorano più di 50 ore */
-
-select 	DISTINCT i.nome,i.cognome,i.cf,l.ore
-from	dipartimento d join impiegato i on d.cf_direttore = i.cf_supervisore
-					   join lavora_su l on i.cf = l.cf_impiegato
-where	l.ore > 250;
